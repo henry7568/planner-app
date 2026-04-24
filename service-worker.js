72b23f5e880cd21222a21960a52effdca12e38c6
unknown <user.name>
2026-04-24 22:49:04 +0900

    fix: improve mobile settings layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "life-planner-v40";
+const CACHE_VERSION = "life-planner-v43";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
